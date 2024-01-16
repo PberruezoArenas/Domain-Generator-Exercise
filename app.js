909adab02a9068da1691caaa3d9a789a98887084
PberruezoArenas <149729949+PberruezoArenas@users.noreply.github.com>
2024-01-16 20:34:47 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let name1 = ['tech', 'web', 'code', 'byte', 'data'];
+let name2 = ['hub', 'site', 'zone', 'world', 'lab'];
+let name3 = ['dom', 'us', 'tech', 'buy', 'old'];
+
+for (let i = 0; i < name1.length; i++) {
+    for ( let j = 0; j < name2.length; j++) {
+        for (let z = 0; z < name3.length; z++){
+            console.log(name1[i]+name2[j]+name3[z]+".es");
+        }
+        
+    }
+
+}
+
